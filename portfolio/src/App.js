3f938404a5bfefa95771459fd5bc8d780351e3b4
emileebogdan <emileebogdan@gmail.com>
2022-11-15 22:20:36 -0700

    set up basic structure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,29 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ portfolio/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navigation() {
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <div className="container">
+        Navigation
+    </div>
+  );
+}

b932eb48ae0c5d87799a2165161d05b52f33489d
emileebogdan <emileebogdan@gmail.com>
2022-12-13 09:19:10 -0700

    project init

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 export default function Project() {
-  // All functional components must have a return method that contains JSX.
-  // We return all the JSX inside a parent element with a className of "container".
   return (
     <div className="container">
         Project
+        {/* component getting mapped to send back 6 applications, image, link to github, link to deployed site */}
     </div>
   );
 }

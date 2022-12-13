596036f938792dcb684d7e9495b76c76187330f8
emileebogdan <emileebogdan@gmail.com>
2022-12-13 09:20:23 -0700

    contact init

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Contact() {
-  // All functional components must have a return method that contains JSX.
-  // We return all the JSX inside a parent element with a className of "container".
+export default function Contact
+() {
   return (
     <div className="container">
         Contact
+        {/* form with entries for name/email/message when the cursor unfocuses from the form see that the field is required, validation on email address */}
     </div>
   );
 }

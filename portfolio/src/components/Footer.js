af9e5350da2d8abf2bfa544e357b2fd21b8876fb
emileebogdan <emileebogdan@gmail.com>
2022-12-13 09:18:01 -0700

    footer init

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 export default function Footer() {
-  // All functional components must have a return method that contains JSX.
-  // We return all the JSX inside a parent element with a className of "container".
   return (
     <div className="container">
         Footer
+        {/* text or icon links to github or linkedin third platform of your choice */}
     </div>
   );
 }

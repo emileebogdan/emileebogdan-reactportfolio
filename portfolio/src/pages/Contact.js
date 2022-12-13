bfcb2204ec4e5cc7463f1ba3a8218aa10d446bd4
emileebogdan <emileebogdan@gmail.com>
2022-12-13 10:11:37 -0700

    moving file structure

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export default function Contact
-() {
+  () {
   return (
     <div className="container">
-        Contact
-        {/* form with entries for name/email/message when the cursor unfocuses from the form see that the field is required, validation on email address */}
+      Contact
+      <form className='contact'>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder='Email' required />
+        <textarea placeholder="Let's Chat"/>
+        <button type="submit">Submit</button>
+      </form>    
     </div>
   );
 }

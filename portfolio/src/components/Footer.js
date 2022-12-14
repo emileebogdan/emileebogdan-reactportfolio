99541ba878603ead71c74801e4a62f852aa627fb
emileebogdan <emileebogdan@gmail.com>
2022-12-13 19:44:51 -0700

    about styling

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import githubLogo from '../assets/images/github.png';
 import linkedIn from '../assets/images/linkedIn.png';
 import stackOverflow from '../assets/images/stackoverflowg.png';
-import './css/footer.css';
 
 
 export default function Footer() {
